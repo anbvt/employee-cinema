d40b48037c69ccdde4804dffb95d83eda811c1a7
nguyensontrungkien <nguyentrungkien08010801@gmail.com>
2023-12-11 19:10:22 +0700

    test scan

--- app/(home)/scan/page.tsx
+++ app/(home)/scan/page.tsx
@@ -23,7 +23,7 @@ const ScanPage = () => {
             <div className="grid grid-cols-3 gap-4">
                 <div className="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h1 className="mb-5 text-3xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Màn hình quét</h1>
-                    {/* <QrScanComponent data={data} setData={setData} /> */}
+                    <QrScanComponent data={data} setData={setData} />
                 </div>
                 <span className="col-span-2"><DescriptionComponent title="Thông tin vé" data={dataBill} /></span>
             </div>
